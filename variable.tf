96c2287628957e3309c3be44a62678d1b67a7c64
Yevgeni Shapiro <23049337+yevgenis2006@users.noreply.github.com>
2026-01-01 21:51:20 +0200

    update

--- variable.tf
+++ variable.tf
@@ -5,27 +5,5 @@ variable "kubeconfig_path" {
   default     = "~/.kube/config"
 }
 
-variable "anythingllm_host" {
-  description = "Public hostname for AnythingLLM"
-  type        = string
-  default     = "llm.appflex.io"
-}
 
-variable "anythingllm_service_name" {
-  description = "Public hostname for AnythingLLM"
-  type        = string
-  default     = "anything-llm"
-}
-
-variable "anythingllm_service_port" {
-  description = "Public hostname for AnythingLLM"
-  type        = string
-  default     = "3001"
-}
-
-variable "anythingllm_tls_secret" {
-  description = "TLS secret name"
-  type        = string
-  default     = "appflex"
-}
 
