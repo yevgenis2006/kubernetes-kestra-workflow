bdeee5125967386393fbcd97d56fc9fa191839f5
Yevgeni Shapiro <23049337+yevgenis2006@users.noreply.github.com>
2026-01-01 21:53:43 +0200

    update

--- variable.tf
+++ variable.tf
@@ -5,5 +5,28 @@ variable "kubeconfig_path" {
   default     = "~/.kube/config"
 }
 
+variable "kestra" {
+  type = object({
+    mode = string
+
+    deployments = object({
+      standalone = bool
+      webserver  = bool
+      executor   = bool
+      scheduler  = bool
+      worker     = bool
+      indexer    = bool
+    })
+
+    replicas = optional(object({
+      webserver = number
+      executor  = number
+      scheduler = number
+      worker    = number
+      indexer   = number
+    }))
+  })
+}
+
 
 
